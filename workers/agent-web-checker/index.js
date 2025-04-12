30959f7522aa199bd0c4c2022a16b5666f87280f
Gregory G. Pletnev <gregory@pletnev.biz>
2025-04-12 19:27:46 +0200

    test1

--- workers/agent-web-checker/index.js
+++ workers/agent-web-checker/index.js
@@ -1,6 +1,6 @@
 export default {
     async scheduled(event, env, ctx) {
-      console.log("⏱️ Scheduled task triggered at", event.scheduledTime);
+      console.log("⏱️ Scheduled task triggered at:", event.scheduledTime);
       // your job logic here
     }
   };
